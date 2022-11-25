2ff4aeaec2a1cdefc42a9d1422b4c6a106f25110
Arjun Acharya <legendarjun333@gmail.com>
2022-11-25 17:06:58 +0545

    Controller create, views site folder vitra template.blade.php, tyo template.blade.php file lai / dekhai, bootstrap, fontawesome, css, js lai public vitra site folder vitra tiniharuko corresponding folder ma rakhi rw template.blade.php file ma link gario

---
+++ resources/views/site/template.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    {{-- bootstrap css --}}
+    <link rel="stylesheet" href="{{ asset('site/bootstrap/css/bootstrap.css') }}">
+
+    {{-- fontaswesome css --}}
+    <link rel="stylesheet" href="{{ asset('site/fontawesome/css/all.css') }}">
+
+    {{-- custom css --}}
+    <link rel="stylesheet" href="{{ asset('site/css/style.css') }}">
+</head>
+<body>
+    <button class="btn btn-danger"><i class="fa-solid fa-play"></i></button>
+
+
+
+
+
+
+    {{-- bootstrap js --}}
+    <script src="{{ asset('site/bootstrap/js/bootstrap.js') }}"></script>
+
+    {{-- fontawesome js --}}
+    <script src="{{ asset('site/fontawesome/js/all.js') }}"></script>
+
+    {{-- custom js --}}
+    <script src="{{ asset('site/js/script.js') }}"></script>
+</body>
+</html>
